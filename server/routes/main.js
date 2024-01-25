b70acaee396d469221df15fbd330bbb4abed84cb
Arkambish <arkambhm.21@uom.lk>
2024-01-25 16:57:06 +0530

    blog app

---
+++ server/routes/main.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+
+router.get("", async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJS Blog",
+      description: "MERN",
+    };
+    let perPage = 10;
+    let page = req.query.page || 1;
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasnextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasnextPage ? nextPage : null,
+      currentRoute: "/",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: "MERN",
+      currentRoute: `/post/${slug}`,
+    };
+    res.render("post", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/about", (req, res) =>
+  res.render("about", {
+    currentRoute: "/about",
+  })
+);
+router.get("/contact", (req, res) =>
+  res.render("contact", {
+    currentRoute: "/contact",
+  })
+);
+router.post("/search", async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "MERN",
+    };
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+      ],
+    });
+
+    res.render("search", {
+      locals,
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
